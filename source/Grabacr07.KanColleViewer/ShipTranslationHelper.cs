b66ed1f20286676a308cdd982248d0d1641e59ca
manzilla <the_best_flash@hotmail.com>
2015-09-01 20:56:35 -0700

    Translate most/all ships.
    Change ship translation format to a format that will automatically support any kai or kai ni of a base ship name.

--- source/Grabacr07.KanColleViewer/ShipTranslationHelper.cs
+++ source/Grabacr07.KanColleViewer/ShipTranslationHelper.cs
@@ -4,21 +4,14 @@
     {
         public static string TranslateShipName(int id, string name)
         {
-            string translated = null;
+            string fullStrippedName = KanColleWrapper.TranslationHelper.StripInvalidCharacters(name);
+            string stripped = KanColleWrapper.TranslationHelper.StripKaiNi(fullStrippedName);
 
-            translated = Translation.Ships.Resources.ResourceManager.GetString(string.Format("Ship{0}", id), Translation.Ships.Resources.Culture);
-
-            if (string.IsNullOrWhiteSpace(translated))
-            {
-                string fullStrippedName = KanColleWrapper.TranslationHelper.StripInvalidCharacters(name);
-                string stripped = KanColleWrapper.TranslationHelper.StripKaiNi(fullStrippedName);
-
-                translated = (string.IsNullOrEmpty(stripped) ? null : Translation.Ships.Resources.ResourceManager.GetString(stripped, Translation.Ships.Resources.Culture));
+            string translated = (string.IsNullOrEmpty(stripped) ? null : Translation.Ships.Resources.ResourceManager.GetString(stripped, Translation.Ships.Resources.Culture));
             
-                if (!string.IsNullOrWhiteSpace(translated))
-                {
-                    translated = KanColleWrapper.TranslationHelper.AppendKaiNi(fullStrippedName, translated, Properties.Resources.Kai, Properties.Resources.Ni);
-                }
+            if (!string.IsNullOrWhiteSpace(translated))
+            {
+                translated = KanColleWrapper.TranslationHelper.AppendKaiNi(fullStrippedName, translated, Properties.Resources.Kai, Properties.Resources.Ni);
             }
 
             return (string.IsNullOrEmpty(translated) ? name : translated);
