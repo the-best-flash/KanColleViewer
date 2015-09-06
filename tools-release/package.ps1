17466647cbb514c2fa087efd98b9d6068e0d0a54
manzilla <the_best_flash@hotmail.com>
2015-09-06 08:04:31 -0700

    Change zip file to have more version information.

--- tools-release/package.ps1
+++ tools-release/package.ps1
@@ -35,7 +35,7 @@
             if ((Test-Path $versionSource) -and (Test-Path $target))
             {
                 $version = (Get-ChildItem $versionSource).VersionInfo
-                $result  = $result + '.{0}.{1}' -f $version.ProductMajorPart, $version.ProductMinorPart
+                $result  = $result + '.{0}' -f $version.ProductVersion 
  
                 Rename-Item -NewName $result -Path $target
                 New-ZipCompression -source $(Join-Path $(Get-Location) $result) -destination $(Join-Path $(Get-Location).Path ('./' + $result + '.zip'))
